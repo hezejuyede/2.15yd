f89ab252672a9661e8ffc9fd6385cfa33b7cea32
15744 <1574408801@qq.com>
2019-06-03 12:13:23 +0800

    Default Changelist

--- src/components/equipment/equipmentInspection.vue
+++ src/components/equipment/equipmentInspection.vue
@@ -9,6 +9,25 @@
           <el-input v-model="select_word" placeholder="检索点检记录" class="handle-input mr10" style="width: 200px"></el-input>
         </label>
         <label style="margin-right: 5px;margin-left: 5px">
+          <span>设备</span>
+          <span>:</span>
+          <el-select
+            style="width: 150px"
+            v-model="shebei"
+            clearable
+            filterable
+            allow-create
+            default-first-option
+            placeholder="请选择设备">
+            <el-option
+              v-for="item in shebeiOptions"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id">
+            </el-option>
+          </el-select>
+        </label>
+        <label style="margin-right: 5px;margin-left: 5px">
           <span>时间</span>
           <span>:</span>
           <el-date-picker
@@ -20,7 +39,8 @@
             value-format="yyyy-MM-dd">
           </el-date-picker>
         </label>
-        <el-button type="success" class="handle-del mr10" @click="doSearchJl">查询点检记录</el-button>
+        <el-button type="success" class="handle-del mr10" @click="doSearchJl">查询</el-button>
+        <el-button type="primary" class="handle-del mr10" @click="addDJ">新增点检</el-button>
       </div>
       <div class="handle-content">
         <el-table class="tb-edit"
@@ -36,6 +56,100 @@
         </el-table>
       </div>
     </div>
+
+    <!--新增点检 -->
+    <el-dialog  :visible.sync="jlVisible" :fullscreen="true" :center="true">
+      <div class="closeBtn">
+        <el-button type="danger" @click="jlVisible = false">关闭窗口</el-button>
+      </div>
+      <div class="djDiv">
+        <div class="djDivTop" id="tjdjSelect">
+          <label>
+            <span style="font-size: 30px">设备</span>
+            <span>:</span>
+            <el-select
+              style="width: 250px"
+              v-model="shebei"
+              clearable
+              filterable
+              allow-create
+              default-first-option
+              @change="changeSB"
+              placeholder="请选择设备">
+              <el-option
+                v-for="item in shebeiOptions"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+              </el-option>
+            </el-select>
+          </label>
+        </div>
+        <div class="djDivCenter">
+          <el-table class="tb-edit"
+                    :data="djData"
+                    :header-cell-style="{background:'#A1D0FC',color:'rgba(0, 0, 0, 0.8)',fontSize:'12px'}"
+                    border
+                    height="500"
+                    highlight-current-row
+                    :cell-style="{fontSize:'11px'}"
+                    style="width: 98%;margin: auto">
+            <el-table-column
+              align="center"
+              prop="bujianname"
+              label="检查部位"
+              width="80">
+            </el-table-column>
+            <el-table-column
+              align="center"
+              prop="no"
+              label="No"
+              width="40">
+            </el-table-column>
+            <el-table-column
+              align="center"
+              prop="xiangmu"
+              label="检查项目"
+              width="150">
+            </el-table-column>
+            <el-table-column
+              align="center"
+              prop="neirong"
+              width="200"
+              label="检查内容">
+            </el-table-column>
+            <el-table-column
+              align="center"
+              prop="fangfa"
+              label="检查方法"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              align="center"
+              prop="caozuo"
+              label="点检操作">
+              <template scope="scope">
+                <div>
+                  <el-radio v-model="scope.row.jieguo" label="1" border>OK</el-radio>
+                  <el-radio v-model="scope.row.jieguo" label="2" border>不良</el-radio>
+                  <el-radio v-model="scope.row.jieguo" label="3" border>维修中</el-radio>
+                </div>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+        <div class="closeBottom">
+          <el-button type="primary" @click="addDJjL">提交点检</el-button>
+        </div>
+      </div>
+
+    </el-dialog>
+
+
+
+
+
+
     <div class="loading-container" v-show="!img.length">
       <loading></loading>
     </div>
@@ -58,8 +172,9 @@
       return {
         img: "",
         userId:"",
+        radio1:"3",
 
-
+        jlVisible:false,
 
         examineTime:"",
         select_word:"",
@@ -67,6 +182,12 @@
         cols: [],
         tableData: [],
 
+        djData:[],
+        djCols:[],
+
+        shebei:"",
+        shebeiOptions:"",
+        qbdj:false,
 
         message: '',
         HideModal: true
@@ -120,15 +241,12 @@
 
           let that = this;
           axios.all([
-            axios.post(" " + url + "/sys/dictionaryList", {"id": "9"}),
-            axios.post(" " + url + "/api/getPersonProcessList", {"name": ""}),
+            axios.post(" " + url + "/shebei/shebeiList", {"jiagongxian": "","stationid":that.workStation})
           ])
-            .then(axios.spread(function (line,workStation) {
-              that.lineOptions = line.data;
-              that.line = line.data[0].indexno;
-              that.workStation = workStation.data[0].id;
-              that.workStationOptions = workStation.data;
-              that.loadingShowData(1);
+            .then(axios.spread(function (shebei) {
+              that.shebei =shebei.data[0].id;
+              that.shebeiOptions = shebei.data;
+              that.loadingShowData(that.shebei)
             }));
 
 
@@ -140,8 +258,8 @@
       loadingShowData(data) {
         let that = this;
         axios.all([
-          axios.post(" " + url + "/sys/showTableTitle", {"name": "sbgzclgz"}),
-          axios.post(" " + url + "/padShow/buttonList", {"id": data})
+          axios.post(" " + url + "/sys/showTableTitle", {"name": "zxdsbdjjlcx"}),
+          axios.post(" " + url + "/shebei/contentListByShebei", {"shebeid": data})
         ])
           .then(axios.spread(function (title, table) {
             that.cols = title.data;
@@ -156,25 +274,50 @@
 
       //根据时间查询上报记录
       doSearchJl(){
-
+        axios.post(" " + url + "/shebei/insertRecord", {
+          "userId": this.userId,
+          "errorId":this.equipment,
+          "context":this.remarks,
+          "id":id
+        })
+          .then((res) => {
+            if (res.data === "1") {
+              this.$message.success(`提交成功`);
+            }
+            else {
+              this.$message.warning(`提交失败`);
+            }
+          })
+          .catch((err) => {
+          })
       },
 
-      //显示上报异常弹出框
-      showYc() {
-        this.sbVisible = true;
-        this.equipment = "";
-        this.remarks = "";
+      addDJ() {
+        this.jlVisible = true;
+        let that = this;
+        axios.all([
+          axios.post(" " + url + "/sys/showTableTitle", {"name": "zxddjbt"}),
+          axios.post(" " + url + "/shebei/contentListByShebei", {"shebeid": this.shebei})
+        ])
+          .then(axios.spread(function (title, table) {
+            that.djCols = title.data;
+            that.djData = table.data.data;
+          }));
+
       },
-
-      //上报设备异常
-      submitAbnormal() {
-        if (this.equipment && this.remarks) {
-
-          axios.post(" " + url + "/shengchanError/errorEvent", {
-            "userId": this.userId,
-            "errorId":this.equipment,
-            "context":this.remarks,
-            "id":id
+      addDJjL(){
+        for (let i = 0; i < this.djData.length; i++) {
+          if (this.djData[i].jieguo === null) {
+            this.qbdj = false;
+          }
+          else {
+            this.qbdj = true;
+          }
+        }
+        if(this.qbdj === true){
+          axios.post(" " + url + "/shebei/insertRecord", {
+            "shebeid": this.shebei,
+            "list":this.djData,
           })
             .then((res) => {
               if (res.data === "1") {
@@ -187,12 +330,22 @@
             .catch((err) => {
             })
         }
-        else if (!this.equipment) {
-          this.$message.warning(`异常设备必须选择`);
+        else {
+          this.$message.warning(`请全部点检才能提交`);
         }
-        else if (!this.remarks) {
-          this.$message.warning(`必须输入设备出现什么故障`);
-        }
+      },
+
+      changeSB(){
+        let that = this;
+        axios.all([
+          axios.post(" " + url + "/sys/showTableTitle", {"name": "zxddjbt"}),
+          axios.post(" " + url + "/shebei/contentListByShebei", {"shebeid": this.shebei})
+        ])
+          .then(axios.spread(function (title, table) {
+            that.djCols = title.data;
+            that.djData = table.data.data;
+          }));
+
       }
     }
   }
@@ -213,9 +366,9 @@
           display: inline-block;
         }
         .el-button {
-          width:150px;
+          width:80px;
           height: 40px;
-          font-size: @font-size-large;
+          font-size: @font-size-medium;
         }
       }
     }
@@ -280,6 +433,54 @@
       }
     }
   }
+
+  .djDiv{
+    width: 100%;
+    height: 100%;
+    .djDivTop{
+      height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .el-button {
+        width:100px;
+        height: 40px;
+        font-size: @font-size-medium;
+      }
+    }
+    .closeBottom{
+      height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .el-button {
+        width:100px;
+        height: 40px;
+        font-size: @font-size-medium;
+      }
+    }
+  }
+
+  .closeBtn {
+    width: 100%;
+    height: 70px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: -8px;
+    left: 0;
+    z-index: 999;
+    .el-button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 30%;
+      height: 50px;
+    }
+  }
+
   .loading-container {
     position: absolute;
     width: 100%;
