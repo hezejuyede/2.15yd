d13f8eb935c12caca3bcaf78a2c83c86f602eefb
15744 <1574408801@qq.com>
2019-06-16 13:26:58 +0800

    Default Changelist

--- src/common/header.vue
+++ src/common/header.vue
@@ -318,11 +318,13 @@
 
       //查看安全提醒
       goToAQ() {
+        localStorage.setItem("IndexUrl", 3);
         this.$router.push("/aqtixiangxuexi")
       },
 
       //查看质量
       goToZl() {
+        localStorage.setItem("IndexUrl", 4);
         this.$router.push("/tixiangxuexi")
       },
 
