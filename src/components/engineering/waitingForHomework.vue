7c5824835e82c9aa4f0435d79c3aaea1c54a7d58
15744 <1574408801@qq.com>
2019-07-16 17:56:50 +0800

    Default Changelist

--- src/components/engineering/waitingForHomework.vue
+++ src/components/engineering/waitingForHomework.vue
@@ -18,7 +18,7 @@
           </div>
           <div class="listSearchBtn">
             <button @click="showScreening">条件筛选</button>
-            <button @click="materialStatistics">直管物料统计</button>
+            <button @click="materialStatistics">物料统计</button>
             <button @click="goGeneralListOfProcessing">总清单</button>
             <button @click="workEnd">报完工</button>
           </div>
@@ -1690,7 +1690,7 @@
       <div class="closeBtn">
         <el-button type="danger" @click="closeMaterielVisible">关闭窗口</el-button>
       </div>
-      <div class="materielContainer" v-if="this.listType ==1 || this.listType ==5 || this.listType ==4 || this.listType ==3 ||this.listType ==11">
+      <div class="materielContainer" v-if="this.listType ==1 || this.listType ==5 || this.listType ==4 || this.listType ==3">
         <div class="materielTop">
           <div class="materielTopDiv">
             <el-select
@@ -2093,6 +2093,106 @@
         </div>
       </div>
       <div class="materielContainer" v-if="this.listType ==8">
+        <div class="materielTop">
+          <div class="materielTopDiv">
+            <el-select
+              v-model="batch"
+              clearable
+              filterable
+              allow-create
+              default-first-option
+              placeholder="批次">
+              <el-option
+                v-for="item in batchOptions"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+              </el-option>
+            </el-select>
+            <el-button type="primary" @click="doSearchMateriel">查询</el-button>
+          </div>
+        </div>
+        <div class="materielCenter">
+          <el-table
+            :data="materielData"
+            height="520"
+            ref="Table"
+            :header-cell-style="{
+            background:'#ffffff',
+            border: '1px solid #303133',
+            fontSize:'18px',
+            color:'rgba(0, 0, 0, 1)'}"
+            :cell-style="{
+             border: '1px solid #303133',
+             fontSize:'16px'
+            }"
+            style="width: 100%;border: 1px solid #303133">
+            <el-table-column
+              align="center"
+              label="出力日">
+              <el-table-column
+                width="80"
+                align="center"
+                prop="koujing"
+                label="口径">
+              </el-table-column>
+            </el-table-column>
+            <el-table-column
+              align="center"
+              :label="batch">
+              <el-table-column
+                align="center"
+                prop="guige"
+                label="规格">
+              </el-table-column>
+              <el-table-column
+                align="center"
+                prop="zhuwenhao"
+                label="注文号"
+                width="300">
+              </el-table-column>
+            </el-table-column>
+            <el-table-column
+              align="center"
+              label="作业者">
+              <el-table-column
+                align="center"
+                width="80"
+                prop="allnum"
+                label="总数量">
+              </el-table-column>
+            </el-table-column>
+            <el-table-column
+              prop="qieduanchang"
+              align="center"
+              :label="zuoyezhe">
+              <el-table-column
+                prop="usednum"
+                align="center"
+                label="已拿数量">
+                <template scope="scope">
+                  <div>
+                    <el-input v-model="scope.row.usednum" type="number" label="1" border @change="inputChangeMateriel(scope.$index)">选中</el-input>
+                  </div>
+                </template>
+              </el-table-column>
+              <el-table-column
+                prop="usedflag"
+                align="center"
+                label="全部拿取">
+                <template scope="scope">
+                  <el-radio v-model="scope.row.usedflag" label="1" border @change="allChangeMateriel(scope.$index,scope.row.usedflag)">否</el-radio>
+                  <el-radio v-model="scope.row.usedflag" label="2" border @change="allChangeMateriel(scope.$index,scope.row.usedflag)">是</el-radio>
+                </template>
+              </el-table-column>
+            </el-table-column>
+          </el-table>
+        </div>
+        <div class="materielBottom">
+          <el-button type="success" @click="doSaveMateriel">确认</el-button>
+        </div>
+      </div>
+      <div class="materielContainer" v-if="this.listType ==11">
         <div class="materielTop">
           <div class="materielTopDiv">
             <el-select
@@ -3504,7 +3604,7 @@
       //查询物料
       doSearchMateriel() {
         if (this.batch) {
-          if (this.dqgw === "直管焊" || this.dqgw === "枝管切断" || this.dqgw === "切断" || this.dqgw === "弯头焊" | this.dqgw === "弯头切断") {
+          if (this.dqgw === "直管焊" || this.dqgw === "枝管切断" || this.dqgw === "切断" || this.dqgw === "弯头焊") {
             this.getMaterielData(2, this.batch);
           }
           else if (this.dqgw === "短管焊") {
@@ -3601,6 +3701,31 @@
                 console.log(err)
               })
           }
+          else if (this.dqgw === "弯头切断") {
+            axios.post(" " + url + "/wuliaotongji/getWuliaoWtqdList",
+              {
+                "type": this.gwType,
+                "pici": this.batch,
+              })
+              .then((res) => {
+                if (res.data.state === "1") {
+                  if (res.data.data.length > 0) {
+                    this.materielData = res.data.data;
+                    this.materielVisible = true;
+                  }
+                  else {
+                    this.materielVisible = true;
+                    this.$message.warning("暂无数据");
+                  }
+                }
+                else {
+                  this.$message.warning(res.data.message);
+                }
+              })
+              .catch((err) => {
+                console.log(err)
+              });
+          }
           else {
             this.$message.success("添加成功");
           }
@@ -3613,7 +3738,7 @@
       //保存物料
       doSaveMateriel() {
         if(this.materielData.length>0){
-          if (this.dqgw === "直管焊" ||this.dqgw === "枝管切断" ||this.dqgw === "切断" ||this.dqgw === "弯头焊" |this.dqgw === "弯头切断") {
+          if (this.dqgw === "直管焊" ||this.dqgw === "枝管切断" ||this.dqgw === "切断" ||this.dqgw === "弯头焊") {
             axios.post(" " + url + "/wuliaotongji/saveWuliaoTongji",
               {
                 "pici":this.batch,
@@ -3659,26 +3784,51 @@
 
           }
           else if (this.dqgw === "小组立") {
-            axios.post(" " + url + "/wuliaotongji/saveWuliaoXzl",
-              {
-                "pici":this.batch,
-                "list": this.materielData,
-                "zuoyezhe": this.zuoyezhe,
-                "type":this.gwType
-              })
-              .then((res) => {
-                if (res.data.state === "1") {
-                  this.materielData=[];
-                  this.$message.success("添加成功");
-                  this.materielVisible = false;
-                }
-                else {
-                  this.$message.warning(res.data.message);
-                }
-              })
-              .catch((err) => {
-                console.log(err)
-              });
+            if(this.gwType==="1"){
+              axios.post(" " + url + "/wuliaotongji/saveWuliaoXzl",
+                {
+                  "pici":this.batch,
+                  "list": this.materielData,
+                  "zuoyezhe": this.zuoyezhe,
+                  "type":this.gwType
+                })
+                .then((res) => {
+                  if (res.data.state === "1") {
+                    this.materielData=[];
+                    this.$message.success("添加成功");
+                    this.materielVisible = false;
+                  }
+                  else {
+                    this.$message.warning(res.data.message);
+                  }
+                })
+                .catch((err) => {
+                  console.log(err)
+                });
+            }
+            else if(this.gwType==="2"){
+              axios.post(" " + url + "/wuliaotongji/saveWuliaoPf",
+                {
+                  "pici":this.batch,
+                  "list": this.materielData,
+                  "zuoyezhe": this.zuoyezhe,
+                  "type":this.gwType
+                })
+                .then((res) => {
+                  if (res.data.state === "1") {
+                    this.materielData=[];
+                    this.$message.success("添加成功");
+                    this.materielVisible = false;
+                  }
+                  else {
+                    this.$message.warning(res.data.message);
+                  }
+                })
+                .catch((err) => {
+                  console.log(err)
+                });
+            }
+
           }
           else if (this.dqgw === "43/48装配") {
             axios.post(" " + url + "/sys/getPiciList")
@@ -3745,6 +3895,28 @@
               .catch((err)=>{
                 console.log(err)
               })
+          }
+          else if (this.dqgw === "弯头切断") {
+            axios.post(" " + url + "/wuliaotongji/saveWuliaoWtqd",
+              {
+                "pici":this.batch,
+                "list": this.materielData,
+                "zuoyezhe": this.zuoyezhe,
+                "type":"1"
+              })
+              .then((res) => {
+                if (res.data.state === "1") {
+                  this.materielData=[];
+                  this.$message.success("添加成功");
+                  this.materielVisible = false;
+                }
+                else {
+                  this.$message.warning(res.data.message);
+                }
+              })
+              .catch((err) => {
+                console.log(err)
+              });
           }
           else {
             this.$message.success("添加成功");
